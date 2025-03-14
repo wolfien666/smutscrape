a3eea32e8582e6b6f7cc0a6a238b23283d6b1691
io-flux <io-flux-email@example.com>
2025-03-13 17:09:47 -0700

    Maintenance

--- regtest2.py
+++
@@ -1,5 +0,0 @@
-from datetime import datetime
-date_str = "2017-09-03T17:49:03+00:00"
-date_format = "%Y-%m-%dT%H:%M:%S%z"
-parsed = datetime.strptime(date_str, date_format).strftime('%Y-%m-%d')
-print(parsed)  # Outputs: 2017-09-03
